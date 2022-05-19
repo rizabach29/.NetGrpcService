ead5ff8f18ed2e48d6212dfafa5c1ed11e0c3be0
Riza Bachtiar <riza.bachtiar.rb@gmail.com>
2022-05-19 09:20:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+using GrpcService2.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddGrpcReflection();
+
+builder.Services.AddCors(o =>
+    o.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("*")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .WithExposedHeaders(
+                    "Grpc-Status",
+                    "Grpc-Message",
+                    "Grpc-Encoding",
+                    "Grpc-Accept-Encoding");
+    }));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("Application:Secret").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            RequireExpirationTime = false,
+        };
+    });
+
+var app = builder.Build();
+IWebHostEnvironment env = app.Environment;
+
+// Configure the HTTP request pipeline.
+
+app.UseRouting();
+
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+
+app.UseCors();
+
+//app.UseAuthentication();
+
+//app.UseAuthorization();
+
+if (env.IsDevelopment())
+    app.MapGrpcReflectionService();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<UserService>();
+});
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcService2;
+
+namespace GrpcService2.Services
+{
+    public class UserService : ExamplePhotoService.ExamplePhotoServiceBase
+    {
+        public override Task<User> CreateUser(User request, ServerCallContext context)
+        {
+            return Task.FromResult(request);
+        }
+
+        public override Task<Empty> UploadPhoto(IAsyncStreamReader<PhotoDataBlock> requestStream, ServerCallContext context)
+        {
+            return base.UploadPhoto(requestStream, context);
+        }
+
+        public override async Task StreamPhotos(IAsyncStreamReader<GetPhotoRequest> requestStream, IServerStreamWriter<Photo> responseStream, ServerCallContext context)
+        {
+            int i = 0;
+            await foreach (var photo in requestStream.ReadAllAsync())
+            {
+                Console.WriteLine(photo.Name);
+
+                await responseStream.WriteAsync(new Photo()
+                {
+                    Name = photo.Name,
+                    DisplayName = $"Photos-Name-{i}",
+                });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                i++;
+            }
+        }
+
+        public override async Task GetPhotos(Empty request, IServerStreamWriter<Photo> responseStream, ServerCallContext context)
+        {
+            var i = 0;
+
+            while (i <= 100)
+            {
+                await responseStream.WriteAsync(new Photo()
+                {
+                    Name = $"Photos-{i}",
+                    DisplayName = $"Photos-Name-{i}",
+                });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                i++;
+            }
+        }
+    }
+}
