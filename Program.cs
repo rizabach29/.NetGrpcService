9fb574b04c00535a00ae0a5e5441877dabae5544
Riza Bachtiar <61179373+rizabach29@users.noreply.github.com>
2023-12-08 15:06:04 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
+// Mantap2 yahut
 // Add services to the container.
 builder.Services.AddGrpc();
 
